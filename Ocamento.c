709588cd46d52cd77575c4ad116e511f05cd4fc1
danificadissima <52755198+danificadissima@users.noreply.github.com>
2020-02-18 20:30:46 -0300

    Create Ocamento.c

---
+++ Ocamento.c
@@ -0,0 +1,18 @@
+// Faça um algoritmo para calcular  quantos metros de madeira serão necessários e o preço a pagar para emoldurar um quadro cujas dimensões são fornecidas pelo usuário. Sabe-se que o preço do metro da madeira custa R$ 5,00
+
+#include <stdio.h>
+
+float tAltura, tBase, result;
+float metro = 5.00;
+
+int main(void) {
+  printf("Digite a alturado quadro: %f \n", tAltura); 
+  scanf("%f",  &tAltura);
+  printf("Digite o tamanho do quadro: %f\n", tBase);
+  scanf("%f", &tBase);
+
+  result = (tAltura * tBase) *5;
+  printf("O preço ficou em: %f\n", tBase);
+
+  return (0);
+}
