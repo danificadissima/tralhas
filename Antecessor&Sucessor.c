061bbe087e6fa3fcd6e3c93a74d40fe452e0f5e6
danificadissima <52755198+danificadissima@users.noreply.github.com>
2020-03-01 21:26:58 -0300

    Create Antecessor&Sucessor.c

---
+++ Antecessor&Sucessor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+int numero, r1, r2;
+printf("\n Digite um numero:");
+scanf("%d",&numero);
+r1= numero + 1;
+r2= numero - 1;
+printf("\n O antecessor é: %d",r2);
+printf("\n O sucessor é: %d",r1);
+return (0);
+
+}
