716f4d4cfc8ce2027cf247dfb3a5749c7958e1bb
danificadissima <52755198+danificadissima@users.noreply.github.com>
2020-03-03 17:13:54 -0300

    Update ReajustedaPassagem.c

--- ReajustedaPassagem.c
+++ ReajustedaPassagem.c
@@ -1,4 +1,5 @@
-//Faça um programa que receba o valor da passagem de ônibus atual calcule e mostre o novo valor, sabendo que sofreu um reajuste de 12%
+//Faça um programa que receba o valor da passagem de ônibus atual
+//calcule e mostre o novo valor, sabendo que sofreu um reajuste de 12%
 
 #include <stdio.h>
 
@@ -12,6 +13,26 @@ float reajuste = 0.12;
   vAtual = (vAntigo * reajuste) + vAntigo;
 
 printf("O valor atual da passagem é R$ %.2f:\n", vAtual);
+
+  return 0;
+}
+
+//Faça um programa que receba  os valores antigo e novo  da passagem de ônibus mostrando o percentual de aumento ( novo-velho)/velho
+
+#include <stdio.h>
+
+int main(void) 
+{
+float vAntigo, vAtual, reajuste;
+
+  printf("Digite o valor antigo da passagem\n");
+   scanf("%f", &vAntigo);
+  printf("Digite o valor novo da passagem\n");
+   scanf("%f", &vAtual);
+  //reajuste = (vAtual - vAntigo) / vAntigo;
+  reajuste = ((vAtual / vAntigo) * 100) - 100;
+
+printf("O percentual de aumento foi de %f:\n", reajuste);
 
   return 0;
 }
